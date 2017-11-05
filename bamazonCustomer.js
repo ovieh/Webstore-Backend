e1b34bd26eeae7fb28a0f7a9ed7f27929adeeee1
Ovieh <ovieh.mosley@gmail.com>
2017-11-05 17:56:58 -0500

    program allows customer to 'buy' stuff

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,49 +4,87 @@ const Table = require('cli-table');
 
 // instantiate 
 let table = new Table({
-    head: ['Item ID', 'Product Name', 'Department', 'Price'],
-    colWidths: [10, 25, 20, 10]
+	head: ['Item ID', 'Product Name', 'Department', 'Price'],
+	colWidths: [10, 25, 20, 10]
 });
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+	host: "localhost",
+	port: 3306,
 
-    // Your username
-    user: "root",
+	// Your username
+	user: "root",
 
-    // Your password
-    password: "Jaxon17",
-    database: "bamazon"
+	// Your password
+	password: "Jaxon17",
+	database: "bamazon"
 });
 
 
 const customerPrompt = () => {
-    inquirer
-        .prompt([{
-                name: 'product_id',
-                type: 'input',
-                message: 'Please enter a product id: '
-            },
-            {
-                name: 'quantity',
-                type: 'input',
-                message: 'Please enter a quantity: '
-            }
+	inquirer
+		.prompt([{
+				name: 'product_id',
+				type: 'input',
+				message: 'Please enter a product id: ',
+				validate: val => (isNaN(val) === false) ? true : false
+			},
+			{
+				name: 'quantity',
+				type: 'input',
+				message: 'Please enter a quantity: ',
+				validate: val => (isNaN(val) === false) ? true : false
+			}
 
-        ]);
+		]).then(answer => {
+			connection.query(
+				"SELECT stock_quantity FROM products WHERE item_id = ?", answer.product_id, (err, res) => {
+					if (err) console.log(err);
+
+					if (answer.quantity <= res[0].stock_quantity) {
+						updateInventory(res, answer.quantity, answer.product_id);
+					} else {
+						console.log("not enough inventory");
+					}
+					
+				}
+
+			)
+		})
+}
+
+const updateInventory = (product, quantity, id) => {
+	let newQty = product[0].stock_quantity - quantity;
+	console.log(product);
+	const query = connection.query(
+		"UPDATE products SET ? WHERE ?",
+		[
+			{
+				stock_quantity: newQty
+			},
+			{
+				item_id: id
+				
+			}
+		], (err, res) => {
+			if (err) console.log(err);
+			console.log(res.affectedRows + " products updated!\n");
+			
+		}
+	)
+	// console.log(query.sql);
 }
 
 
 const displayProducts = (callback) => {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) console.log(err);
+	connection.query("SELECT * FROM products", function (err, res) {
+		if (err) console.log(err);
 
-        res.map( x =>  table.push([ x.item_id, x.product_name, x.department_name, x.price ]));
+		res.map(x => table.push([x.item_id, x.product_name, x.department_name, x.price]));
 
-        console.log(table.toString());
-        callback();
-    });
+		console.log(table.toString());
+		callback();
+	});
 }
 
 
