67519849bf9af51c76f890bc092c76d680d5adc8
Ovieh <ovieh.mosley@gmail.com>
2017-11-05 22:34:27 -0500

    Works using callbacks. I may want to figure out promises. So I can figure out async-await

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ const Table = require('cli-table');
 // instantiate 
 let table = new Table({
 	head: ['Item ID', 'Product Name', 'Department', 'Price'],
-	colWidths: [10, 25, 20, 10]
+	colWidths: [10, 25, 20, 12]
 });
 
 const connection = mysql.createConnection({
@@ -38,53 +38,81 @@ const customerPrompt = () => {
 
 		]).then(answer => {
 			connection.query(
-				"SELECT stock_quantity FROM products WHERE item_id = ?", answer.product_id, (err, res) => {
-					if (err) console.log(err);
+				"SELECT stock_quantity, price FROM products WHERE item_id = ?", answer.product_id,
+				(err, res) => {
+					if (err) throw err;
 
 					if (answer.quantity <= res[0].stock_quantity) {
-						updateInventory(res, answer.quantity, answer.product_id);
+						updateInventory(res, answer.quantity, answer.product_id, newPurchase);
 					} else {
 						console.log("not enough inventory");
+						newPurchase();
 					}
-					
+
 				}
 
 			)
 		})
 }
 
-const updateInventory = (product, quantity, id) => {
+const updateInventory = (product, quantity, id, callback) => {
 	let newQty = product[0].stock_quantity - quantity;
-	console.log(product);
 	const query = connection.query(
-		"UPDATE products SET ? WHERE ?",
-		[
-			{
+		"UPDATE products SET ? WHERE ?", [{
 				stock_quantity: newQty
 			},
 			{
 				item_id: id
-				
+
 			}
 		], (err, res) => {
-			if (err) console.log(err);
+			if (err) throw err;
+			console.log(`The total cost of your purchase is: ${calculateCost(quantity, product[0].price)}`);
 			console.log(res.affectedRows + " products updated!\n");
-			
+			callback();
 		}
 	)
-	// console.log(query.sql);
+	
 }
+
+// Create our number formatter.
+const formatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+	minimumFractionDigits: 2,
+});
+
+const calculateCost = (quantity, price) => {
+	return formatter.format(quantity * price);
+}
+
 
 
 const displayProducts = (callback) => {
 	connection.query("SELECT * FROM products", function (err, res) {
-		if (err) console.log(err);
+		if (err) throw err;
 
-		res.map(x => table.push([x.item_id, x.product_name, x.department_name, x.price]));
+		res.map(x => table.push([x.item_id, x.product_name, x.department_name, formatter.format(x.price)]));
 
 		console.log(table.toString());
 		callback();
 	});
+}
+
+const newPurchase = () => {
+	inquirer.prompt([{
+			type: 'confirm',
+			name: 'buyAgain',
+			message: 'Would you like to buy something else?',
+			default: false
+		}])
+		.then(answer => {
+			if (answer.buyAgain === true) {
+				displayProducts(customerPrompt);
+			} else {
+				process.exit();
+			}
+		})
 }
 
 
