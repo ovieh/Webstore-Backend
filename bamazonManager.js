26833bdeffea88e854a8a84ab3768ed80369ce59
Ovieh <ovieh.mosley@gmail.com>
2017-11-06 13:19:06 -0500

    Manager view displays inventory

---
+++ bamazonManager.js
@@ -0,0 +1,80 @@
+const inquirer = require("inquirer");
+const mysql = require('mysql');
+const Table = require('cli-table');
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Jaxon17",
+    database: "bamazon"
+});
+
+// Create our number formatter.
+const formatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+	minimumFractionDigits: 2,
+});
+
+const calculateCost = (quantity, price) => {
+	return formatter.format(quantity * price);
+}
+
+
+
+const displayProducts = () => {
+	connection.query("SELECT * FROM products", function (err, res) {
+		if (err) throw err;
+
+		res.map(x => table.push([x.item_id, x.product_name, x.department_name, formatter.format(x.price), x.stock_quantity]));
+
+		console.log(table.toString());
+		// callback();
+	});
+}
+
+// instantiate 
+let table = new Table({
+	head: ['Item ID', 'Product Name', 'Department', 'Price', 'Quantity'],
+	colWidths: [10, 25, 20, 12, 12]
+});
+
+
+const listOptions = () => {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: [
+            'View Products for Sale',
+            'View Low Inventory',
+            'Add to Inventory',
+            'Add New Product'
+        ]
+    }]).then(answer => {
+        switch (answer.choice) {
+            case 'View Products for Sale':
+                displayProducts();
+                break;
+            case 'View Low Inventory':
+                displayLowInventory();
+                break;
+            case 'Add to Inventory':
+                addInventory();
+                break;
+            case 'Add New Product':
+                addProduct();
+                break;
+
+        }
+    })
+}
+
+listOptions();
+
